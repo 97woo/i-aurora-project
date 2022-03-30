70d00f718c8db0175ba8871edf0bf20a7511639b
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-30 18:19:49 +0900

    add:id,password vaildator

--- users/validators.py
+++ users/validators.py
@@ -1,5 +1,9 @@
 import re
 
+def validate_id(trinity_id):
+    id_regex = '^[a-zA-z-0-9]{6,20}$'
+    return re.match(id_regex, trinity_id)
+
 def validate_password(trinity_password):
     password_regex = '^[0-9]{6,6}$'
     return re.match(password_regex, trinity_password)
--- users/views.py
+++ users/views.py
@@ -1,16 +1,18 @@
 from django.http                  import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from rest_framework.views         import APIView
 
-from .serializers              import UserSerializer
-from rest_framework.views      import APIView
+from .serializers import UserSerializer
+from users.models import User
+from rest_framework.parsers import JSONParser
 
 
 class SignUpView(APIView):
     def post(self,request):
             serializer = UserSerializer(data=request.data)
-            
+            print(request)
             if serializer.is_valid():
                 serializer.save()
                 return JsonResponse(serializer.data, status=201)
             return JsonResponse(serializer.errors, status=400)
-            
+
