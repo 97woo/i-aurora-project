b7a20d5c7175afe414b49ff1d41dbc9449d361a9
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-31 16:03:11 +0900

    add:idcheck api

--- users/serializers.py
+++ users/serializers.py
@@ -4,9 +4,6 @@ from .validators    import validate_id , validate_password
 
 class UserSignUpSerializer(serializers.ModelSerializer):
     def validate(self, data):
-        if not validate_id(data['trinity_id']):
-            raise serializers.ValidationError('영문과 숫자로 6자리이상 20자리미만으로 입력해주세요!')
-        
         if not validate_password(data['trinity_password']):
             raise serializers.ValidationError('숫자만으로 6자리를 입력해주세요!')
         return data
