f7add11d1fe14286b72ec30efdca524b5e507419
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-31 15:58:56 +0900

    add:idcheck api

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, include
 from users       import views
+from .views       import id_check
 
 urlpatterns = [
     path('signup/', views.SignUpView.as_view()),
+    path('idcheck/', id_check),
 ]    
